9416db85e6b7668c674c9fd7d9c6b8f9bab2bc94
Jason Gritman <jasongritman@gmail.com>
2017-12-30 12:43:49 -0700

    add pods

---
+++ Example/Pods/Target Support Files/HSAttachmentPicker/HSAttachmentPicker-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_HSAttachmentPicker : NSObject
+@end
+@implementation PodsDummy_HSAttachmentPicker
+@end
---
+++ Example/Pods/Target Support Files/Pods-HSAttachmentPicker_Example/Pods-HSAttachmentPicker_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HSAttachmentPicker_Example : NSObject
+@end
+@implementation PodsDummy_Pods_HSAttachmentPicker_Example
+@end
