6e2728448902bdef24445ccd49577a0f33fa520f
Sam Symons <sam@samsymons.com>
2018-04-23 08:41:09 -0700

    Add an Xcode project for the attachment picker project itself.

---
+++ HSAttachmentPicker/HSAttachmentPicker.h
@@ -0,0 +1,9 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for HSAttachmentPicker.
+FOUNDATION_EXPORT double HSAttachmentPickerVersionNumber;
+
+//! Project version string for HSAttachmentPicker.
+FOUNDATION_EXPORT const unsigned char HSAttachmentPickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HSAttachmentPicker/PublicHeader.h>
