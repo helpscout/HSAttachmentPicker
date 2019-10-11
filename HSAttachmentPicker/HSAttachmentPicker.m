f4334cee4cf771c75002d939086c609d063bdc4d
Scott Petit <petit.scott@gmail.com>
2019-10-10 20:24:23 -0500

    Adds a preferredVideoQuality property to HSAttachmentPickerr so clients can configure it themselves

--- HSAttachmentPicker/HSAttachmentPicker.m
+++ HSAttachmentPicker/HSAttachmentPicker.m
@@ -13,6 +13,14 @@
 
 @implementation HSAttachmentPicker
 
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        self.preferredVideoQuality = UIImagePickerControllerQualityTypeMedium;
+    }
+    return self;
+}
+
 -(void)showAttachmentMenu {
     self.selfReference = self;
     UIAlertController *picker = [UIAlertController alertControllerWithTitle:nil message:nil preferredStyle:UIAlertControllerStyleActionSheet];
@@ -126,7 +134,7 @@
     imagePicker.delegate = self;
     imagePicker.allowsEditing = NO;
     imagePicker.mediaTypes = [[NSArray alloc] initWithObjects:(NSString*)kUTTypeImage, (NSString*)kUTTypeMovie, nil];
-    imagePicker.videoQuality = UIImagePickerControllerQualityTypeLow;
+    imagePicker.videoQuality = self.preferredVideoQuality;
     imagePicker.sourceType = sourceType;
     [self.delegate attachmentPickerMenu:self showController:imagePicker completion:^{
         UIApplication.sharedApplication.statusBarHidden = YES;
